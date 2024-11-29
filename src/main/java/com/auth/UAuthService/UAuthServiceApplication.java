c5a35affa2e11550fa68ea873d19cd7444d75bed
SChavan77 <sushmitha.chavan7@gmail.com>
2024-11-29 20:04:54 +0530

    Init setUp of Auth Service

---
+++ src/main/java/com/auth/UAuthService/UAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.auth.UAuthService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UAuthServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/auth/UAuthService/UAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auth.UAuthService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UAuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
